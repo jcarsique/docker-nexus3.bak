7bde40057f19f7598bf0acf8c93ebaa486265674
Julien Carsique <jcarsique@nuxeo.com>
2019-12-13 19:30:23 +0100

    SUPINT-1574: improve postStart
    remove anonymous post edit (bugged: always false)
    output progress (sleep 5->1) and healthcheck at end
    utils: add maven-ncp start script

---
+++ utils/build_nexus-ncp.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -xe
+#
+# Manual build of Nexus NCP
+#
+# (C) Copyright 2019 Nuxeo SA (http://nuxeo.com/) and contributors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Contributors:
+#   Julien Carsique
+#
+
+export NEXUS3_VERSION=3.19.1
+export SCM_REF=`git id`
+export VERSION=`git rev-parse --symbolic-full-name --abbrev-ref HEAD`
+CUSTOM_VERSION=${CUSTOM_VERSION:-$VERSION}
+export DOCKER_REGISTRY=localhost:5000
+
+echo "Building $CUSTOM_VERSION from Nexus 3 $NEXUS3_VERSION, codebase $VERSION ($(git rev-parse --short HEAD))"
+echo "# Base..."
+docker build --build-arg NEXUS3_VERSION --build-arg SCM_REF -t $DOCKER_REGISTRY/nuxeo/nexus3/base:$VERSION base
+
+echo "# Builder..."
+docker build -t $DOCKER_REGISTRY/nuxeo/nexus3/builder:$VERSION builder
+
+export PARMS=maven-ncp
+export DESCRIPTION="NCP PROD $CUSTOM_VERSION"
+echo "# Nexus..."
+docker build --no-cache --build-arg VERSION --build-arg SCM_REF --build-arg DOCKER_REGISTRY --build-arg PARMS --build-arg DESCRIPTION -t devtools/nexus3/$PARMS:$CUSTOM_VERSION .
+docker tag devtools/nexus3/$PARMS:$CUSTOM_VERSION dockerpriv.nuxeo.com:443/devtools/nexus3/$PARMS:$CUSTOM_VERSION
+docker push dockerpriv.nuxeo.com:443/devtools/nexus3/$PARMS:$CUSTOM_VERSION
---
+++ utils/start_nexus-ncp.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -e
+#
+# Manual init for Nexus NCP.
+#
+# (C) Copyright 2019 Nuxeo SA (http://nuxeo.com/) and contributors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+CONFIG_PATH=/root/nexus-config/
+IMAGE=devtools/nexus3/maven-ncp
+VERSION=${VERSION:-0.0.8}
+NAME=nexus
+docker run --restart=always -p 8081:8081 --name $NAME -v $CONFIG_PATH/:/opt/sonatype/nexus/config/ \
+    -v $CONFIG_PATH/license.lic:/nexus-data/etc/license/.license.lic \
+    --ulimit nofile=65536:65536 -e INSTALL4J_ADD_VM_PARAMS="-Xms4G -Xmx4G -XX:MaxDirectMemorySize=2G" \
+    -itd dockerpriv.nuxeo.com:443/$IMAGE:$VERSION
+
+# curl --output /dev/null --silent --head --fail localhost:8081/service/rest/v1/status
+
+echo "Running post start..."
+docker exec -u nexus $NAME /opt/sonatype/nexus/postStart.sh |tee postStart.log
+clear
+cat postStart.log
+docker logs $NAME |grep ERROR
+
