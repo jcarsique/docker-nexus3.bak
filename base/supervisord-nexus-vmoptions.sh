a1436a882b1e81848a2caf6497fd6ead980e660c
Alexis Timic <atimic@nuxeo.com>
2020-01-15 16:32:02 +0100

    NXBT-3162: fix .java path

--- base/supervisord-nexus-vmoptions.sh
+++ base/supervisord-nexus-vmoptions.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -ex
 
-grep -q '-Djava.util.prefs.userRoot=/opt/sonatype/nexus/.java' /opt/sonatype/nexus/bin/nexus.vmoptions && exit 0
-echo '-Djava.util.prefs.userRoot=/opt/sonatype/nexus/.java' >> /opt/sonatype/nexus/bin/nexus.vmoptions
+grep -q '-Djava.util.prefs.userRoot=/nexus-data/.java' /opt/sonatype/nexus/bin/nexus.vmoptions && exit 0
+echo '-Djava.util.prefs.userRoot=/nexus-data/.java' >> /opt/sonatype/nexus/bin/nexus.vmoptions
