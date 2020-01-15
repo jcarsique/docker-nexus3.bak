5fe0a3f00ad429f9877cf413562a62f77701024c
Julien Carsique <jcarsique@nuxeo.com>
2020-01-15 23:43:51 +0100

    NXBT-3162 port changes from forks
    > 85ca107 2019-08-16 AlienResidents       Quoted variables. Added NEXUS_BASE_DIR, and NEXUS_REPO_DIR vars.  Implemented shellcheck recommendations for using find, mapfile, and a few others.
    > 545ce07 2019-08-16 AlienResidents       Modified arguments requirements for createOrUpdateAndRun to need just the path to the repository. Added -t to mapfile to remove newlines.
    > ee811a7 2020-01-08 Cosmin Cojocar       fix: change correctly the admin password

--- postStart.sh
+++ postStart.sh
@@ -2,10 +2,10 @@
 # Forked from https://github.com/jenkins-x-charts/nexus/blob/v0.1.20/postStart.sh
 
 set -e
-HOST=localhost:8081
+HOST="localhost:8081"
 
 printf 'Waiting for server start...'
-until $(curl --output /dev/null --silent --head --fail http://$HOST/); do
+until $(curl --output /dev/null --silent --head --fail "http://${HOST}/"); do
   printf '.'
   sleep 1
 done
@@ -16,40 +16,42 @@ grep -e ERROR -e WARN /nexus-data/log/nexus.log|head || true
 echo
 
 echo "Configuring..."
+NEXUS_BASE_DIR="/opt/sonatype/nexus"
+SCRIPTS_PATH="${NEXUS_BASE_DIR}/scripts"
+CONFIG_PATH="${NEXUS_BASE_DIR}/config"
 USERNAME="admin"
-SCRIPTS_PATH="/opt/sonatype/nexus/scripts"
-CONFIG_PATH="/opt/sonatype/nexus/config"
 PASSWORD="admin123"
 if [ -f "/nexus-data/admin.password" ]; then
   echo "Read generated admin password"
   PASSWORD="$(cat /nexus-data/admin.password)"
 fi
-PASSWORD_FROM_FILE="$(cat $CONFIG_PATH/password || true)"
+PASSWORD_FROM_FILE="$(cat ${CONFIG_PATH}/password || true)"
 declare -a SCRIPT_LIST=
 
 function die() {
-    echo "ERROR: $@" 1>&2
+    echo "ERROR: $*" 1>&2
     exit 1
 }
 
 function createOrUpdateAndRun() {
-    local scriptName=$1
-    local scriptFile=$2
-    local scriptParms=$3
+    local scriptName=${1}
+    local scriptFile=${2}
+    local scriptParms=${3}
+    local scriptName_regex=" ${scriptName} "
 
-    if [ "${#SCRIPT_LIST[@]}" = 0 ] || [[ ! " ${SCRIPT_LIST[@]} " =~ " ${scriptName} " ]]; then
-        echo "Creating $scriptName script"
-        curl --fail -sS -X POST -u $USERNAME:$PASSWORD --header "Content-Type: application/json" "http://$HOST/service/rest/v1/script/" -d @$scriptFile
+    if [ "${#SCRIPT_LIST[@]}" = 0 ] || [[ ! " ${SCRIPT_LIST[*]} " =~ ${scriptName_regex} ]]; then
+        echo "Creating ${scriptName} script"
+        curl --fail -sS -X POST -u "${USERNAME}":"${PASSWORD}" --header "Content-Type: application/json" "http://${HOST}/service/rest/v1/script/" -d @"${scriptFile}"
     else
         echo "Updating $scriptName script"
-        curl --fail -sS -X PUT -u $USERNAME:$PASSWORD --header "Content-Type: application/json" "http://$HOST/service/rest/v1/script/$scriptName" -d @$scriptFile
+        curl --fail -sS -X PUT -u "${USERNAME}":"${PASSWORD}" --header "Content-Type: application/json" "http://${HOST}/service/rest/v1/script/${scriptName}" -d @"${scriptFile}"
     fi
     echo "Running $scriptName script"
     if [ -z "${scriptParms}" ]; then
-      curl --fail -sS -X POST -u $USERNAME:$PASSWORD --header "Content-Type: application/json" "http://$HOST/service/rest/v1/script/$scriptName/run" \
+      curl --fail -sS -X POST -u "${USERNAME}":"${PASSWORD}" --header "Content-Type: application/json" "http://${HOST}/service/rest/v1/script/${scriptName}/run" \
           |sed 's,\\",",g; s,"{,{,g; s,}",},g'|jq .
     else
-      curl --fail -sS -X POST -u $USERNAME:$PASSWORD --header "Content-Type: application/json" "http://$HOST/service/rest/v1/script/$scriptName/run" -d @$scriptParms \
+      curl --fail -sS -X POST -u "${USERNAME}":"${PASSWORD}" --header "Content-Type: application/json" "http://${HOST}/service/rest/v1/script/${scriptName}/run" -d @"${scriptParms}" \
           |sed 's,\\",",g; s,"{,{,g; s,}",},g'|jq .
     fi
     echo
@@ -57,35 +59,35 @@ function createOrUpdateAndRun() {
 
 function setScriptList() {
     # initialising the scripts already present once and assuming that there no duplicate script names in the scripts that follow
-    SCRIPT_LIST=($(curl --fail -s -u $USERNAME:$PASSWORD http://$HOST/service/rest/v1/script | grep -oE "\"name\" : \"[^\"]+" | sed 's/"name" : "//'))
+    mapfile -t SCRIPT_LIST < <(curl --fail -s -u "${USERNAME}":"${PASSWORD}" "http://${HOST}/service/rest/v1/script" | grep -oE "\"name\" : \"[^\"]+" | sed 's/"name" : "//')
 }
 
 function testLogin() {
-    curl --fail --silent -u $USERNAME:$1 http://$HOST/service/metrics/ping >/dev/null 2>&1
+    curl --fail --silent -u "${USERNAME}":"${1}" "http://${HOST}/service/metrics/ping" >/dev/null 2>&1
 }
 
 function healthcheck() {
     echo "HealthCheck:"
-    curl -v -sS -u $USERNAME:$PASSWORD http://$HOST/service/metrics/healthcheck |jq .
+    curl -v -sS -u "${USERNAME}":"${PASSWORD}" "http://${HOST}/service/metrics/healthcheck" |jq .
 }
 
 # Need password upgrade from file?
 if test -n "$PASSWORD_FROM_FILE" && ! testLogin "$PASSWORD_FROM_FILE"; then
     echo "Setting password from file"
-    createOrUpdateAndRun set_admin_password "$SCRIPTS_PATH/set_admin_password-body.json"
+    createOrUpdateAndRun set_admin_password "${SCRIPTS_PATH}/set_admin_password-body.json"
 fi
 PASSWORD=${PASSWORD_FROM_FILE:-$PASSWORD}
-testLogin "$PASSWORD" || die "Login fails."
+testLogin "${PASSWORD}" || die "Login fails."
 setScriptList
 
 for script in blobstore repository security task_timeout; do
-    body="$SCRIPTS_PATH/${script}-body.json"
+    body="${SCRIPTS_PATH}/${script}-body.json"
     if [ -f "$CONFIG_PATH/${script}.json" ] ; then
         parms="$CONFIG_PATH/${script}.json"
     else
-        parms="$SCRIPTS_PATH/${script}-parms.json"
+        parms="${SCRIPTS_PATH}/${script}-parms.json"
     fi
-    createOrUpdateAndRun ${script} ${body} ${parms}
+    createOrUpdateAndRun "${script}" "${body}" "${parms}"
 done
 echo "Configuration done."
 
